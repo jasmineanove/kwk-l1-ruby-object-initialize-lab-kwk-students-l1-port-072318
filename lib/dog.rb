4ff154c3f4a1e1b358b2a621bd6c834ecee60401
jasmineanove <jasmineanove@gmail.com>
2018-07-27 16:50:05 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
 # dog.rb
+
+
+class Dog
+  # def initialize(name, breed)
+  #   @name = name
+  #   @breed = breed
+  # end
+  
+ def name
+   @name
+   end
+
+ def breed
+   @breed
+   end
+  
+  def name=(new_name)
+   @name = new_name
+  end
+  
+  def breed=(Mutt)
+   @breed = Mutt
+  end
+  
+end
+
